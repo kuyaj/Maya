7566bd7daaafcf2627f62a1fd2430b174813f230
Angelo <valmocinajayangelo@gmail.com>
2018-01-15 14:51:56 +0830

    Update iStore and delete detail folder

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {  Card, Image, Container } from 'semantic-ui-react'
+import {  Card, Container } from 'semantic-ui-react'
 import { observer } from 'mobx-react'
 import '../index.css'
 
@@ -14,22 +14,22 @@ render(){
 
     return (
        <div>
-         
+        <Container >
+        <br/>   
+        <Card.Group itemsPerRow={4}>
          {
-           this.props.store.story.map((stories, i) => {
-            return(
-              <Container >
-               <br/>              
-                <Card.Group itemsPerRow={4}>
-                  <Card>
-                    <Image src={ stories.url } />
-                  </Card>
-                </Card.Group>
-                
-              </Container>
+           this.props.store.story.map((x, y) => {
+            return(                                 
+                  <Card 
+                      key={y}
+                      image={ x.url } 
+                  />
+       
             )
            })
-         }
+         }   
+           </Card.Group>   
+        </Container>
        </div>
     )
   }
--- src/iStore.js
+++ src/iStore.js
@@ -12,15 +12,39 @@ class iStore {
 @observable story = [
 
     {
-        title : "Safe Haven",
-        url : "https://hips.hearstapps.com/sev.h-cdn.co/assets/17/02/980x1452/gallery-1484005021-safe-haven-xlg.jpg?resize=480:*",
-        genra : "Romance"
+        name : "Uncensored",
+        url : "https://a.wattpad.com/cover/38996221-352-k886297.jpg",
     },
     {
-        title : "The Fault In Our Stars",
-        url : "http://www.craigerscinemacorner.com/Images/Fault-in-our-Stars-poster.jpg",
-        genra : "Romance"
+        name : "Love Songs For No One",
+        url : "https://a.wattpad.com/cover/123309734-352-k164130.jpg",
+
+    },
+    {
+        name : "Twenty-three : Eleven",
+        url : "https://a.wattpad.com/cover/20323617-352-k349920.jpg",
+    },
+    {
+        name: "Blown by the Wind",
+        url :  "https://a.wattpad.com/cover/98878170-352-k375004.jpg",
+    },
+    {
+        name: "Stupidly In Love Again",
+        url : "https://a.wattpad.com/cover/81729695-352-k806919.jpg",
+    },
+    {
+        name: "Heartless",
+        url :  "https://a.wattpad.com/cover/8294706-352-k7970.jpg",
+    },
+    {
+        name : 'The Living Arrow',
+        url :  "https://a.wattpad.com/cover/57872844-352-k926810.jpg",
+    },
+    {
+        name: "She Who Stole The Cupid's Arrow ",
+        url :  "https://i.pinimg.com/originals/09/06/06/0906062f68038bcc69c4c01bf692d373.jpg"
     }
+    
 ]
 
 }
